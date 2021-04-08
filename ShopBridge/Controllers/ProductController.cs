98764b92b91c9949f429ef92481476d8682c31ef
ashitosh-git <ashitosh6707@gmail.com>
2021-04-08 08:10:47 +0530

    initial commit

---
+++ ShopBridge/Controllers/ProductController.cs
@@ -0,0 +1,118 @@
+﻿using ShopBridge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopBridge.Controllers
+{
+    public class ProductController : Controller
+    {
+        HttpClient client = new HttpClient();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Display()
+        {
+            List<Product> list = new List<Product>();
+            client.BaseAddress = new Uri("http://localhost:61809/api/webapi/GetData");
+            var consume = client.GetAsync("GetData");
+            consume.Wait();
+            var test = consume.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<List<Product>>();
+                list = display.Result;
+            }
+            return View(list);
+        }
+
+        [HttpGet]
+        public ActionResult InsertData()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult InsertData(Product data)
+        {
+            client.BaseAddress = new Uri("http://localhost:61809/api/webapi/InsertData");
+            var consume = client.PostAsJsonAsync("Insert",data);
+            consume.Wait();
+            var test = consume.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Display");
+            }
+            else {
+                return HttpNotFound();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            Product product = new Product();
+            client.BaseAddress = new Uri("http://localhost:61809/api/webapi/Details");
+            var consume = client.GetAsync("Details?id="+ id.ToString());
+            consume.Wait();
+            var test = consume.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Product>();
+                product = display.Result;
+            }
+            return View(product);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Product product = new Product();
+            client.BaseAddress = new Uri("http://localhost:61809/api/webapi/Delete");
+            var consume = client.DeleteAsync("Delete?id=" + id.ToString());
+            consume.Wait();
+            var test = consume.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Display");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Product product = null;
+            client.BaseAddress = new Uri("http://localhost:61809/api/webapi/Details");
+            var consume = client.GetAsync("Details?id=" + id.ToString());
+            consume.Wait();
+            var test = consume.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Product>();
+                display.Wait();
+                product = display.Result;
+            }
+                return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product data)
+        {
+            client.BaseAddress = new Uri("http://localhost:61809/api/webapi/Update");
+            var consume = client.PutAsJsonAsync<Product>("Update",data);
+            consume.Wait();
+            var test = consume.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Display");
+            }
+            return View();
+        }
+    }
+}
---
+++ ShopBridge/Controllers/WebApiController.cs
@@ -0,0 +1,61 @@
+﻿using ShopBridge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ShopBridge.Controllers
+{
+    public class WebApiController : ApiController
+    {
+        ShopBridgeEntities shopBridge = new ShopBridgeEntities();
+
+        [HttpGet]
+        [Route("api/webapi/GetData")]
+        public IHttpActionResult GetData()
+        {
+            List<Product> list = shopBridge.Products.ToList();
+            return Ok(list);
+
+        }
+
+        [HttpPost]
+        public IHttpActionResult Insert(Product product)
+        {
+            shopBridge.Products.Add(product);
+            shopBridge.SaveChanges();
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("api/webapi/Details")]
+        public IHttpActionResult Details(int id)
+        {
+            Product data = shopBridge.Products.Where(x => x.Id == id).SingleOrDefault();
+            return Ok(data);
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            Product data = shopBridge.Products.Where(x => x.Id == id).SingleOrDefault();
+            shopBridge.Products.Remove(data);
+            shopBridge.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPut]
+        public IHttpActionResult Update(Product product)
+        {
+            Product data = shopBridge.Products.Where(x => x.Id == product.Id).SingleOrDefault();
+            data.Name = product.Name;
+            data.ProductId = product.ProductId;
+            data.Description = product.Description;
+            data.Price = product.Price;
+            shopBridge.SaveChanges();
+            return Ok();
+        }
+    }
+}
